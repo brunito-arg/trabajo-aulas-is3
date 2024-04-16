3eb943587671f898188c224efaec7ffb490041c0
= <=>
2024-04-16 20:17:00 -0300

    creacion proyecto

---
+++ trabajo-aulas-is3/src/main/java/com/grupo2/trabajoaulasis3/TrabajoAulasIs3Application.java
@@ -0,0 +1,13 @@
+package com.grupo2.trabajoaulasis3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoAulasIs3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoAulasIs3Application.class, args);
+	}
+
+}
---
+++ trabajo-aulas-is3/src/test/java/com/grupo2/trabajoaulasis3/TrabajoAulasIs3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo2.trabajoaulasis3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoAulasIs3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
